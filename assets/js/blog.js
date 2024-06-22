737a14b9eaeabf3c85eb1408109230445d905cf9
Yanique Brooks <yaniquebro@gmail.com>
2024-06-22 10:39:58 -0400

    blog.html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,28 @@
+const blogTitle = document.querySelector(".post-title")
+const blogContent = document.querySelector(".post-content")
+const postedBy = document.querySelector(".username")
+const blogSection = document.querySelector("#blog-entries")
 
+
+// Gets the user information from local storage 
+function displayFormInfo(){
+
+    const displayInfo = JSON.parse(localStorage.getItem("userInfo"))
+
+    console.log(displayInfo)
+
+    displayInfo.forEach(index => {
+
+        // Displays the informattion from each item in the array in local storage
+        blogSection.innerHTML += `
+        <section>
+        <h3 class="blog-title-1">${index.userTitle}</h3>
+        <p class="content">${index.userContent}</p>
+        <p class="posted-by">Posted By: ${index.user}</p>
+        </section>
+        `
+    });
+}
+
+
+displayFormInfo();
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,30 +1,62 @@
-function addToSeries (e) {
+// function addToSeries (e) {
 
-    e.preventDefault();
+//     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
-    const postTitle = document.getElementById("post-title").value.trim();
-    const postContent = document.getElementById("blog-post").value.trim();
+//     const username = document.getElementById("username").value.trim();
+//     const postTitle = document.getElementById("post-title").value.trim();
+//     const postContent = document.getElementById("blog-post").value.trim();
 
-    let blogPost = {
-        username: username,
-        title: postTitle,
-        content: postContent
-    }
-    console.log(blogPost)
+//     let blogPost = {
+//         username: username,
+//         title: postTitle,
+//         content: postContent
+//     }
+//     console.log(blogPost)
 
-    if(localStorage.getItem('blogEntries') == null) {
-        localStorage.setItem('blogEntries', '[]');
-    }    
+//     if(localStorage.getItem('blogEntries') == null) {
+//         localStorage.setItem('blogEntries', '[]');
+//     }    
 
-    const allEntries = JSON.parse(localStorage.getItem('blogEntries'));
+//     const allEntries = JSON.parse(localStorage.getItem('blogEntries'));
     
-    allEntries.push(blogPost);
-    console.log(`updated array: ${allEntries}`)
+//     allEntries.push(blogPost);
+//     console.log(`updated array: ${allEntries}`)
 
-    localStorage.setItem('blogEntries', JSON.stringify(allEntries));
+//     localStorage.setItem('blogEntries', JSON.stringify(allEntries));
 
-    window.location.href = './blog.html';
+//     window.location.href = './blog.html';
+// }
+
+// document.getElementById('submit').addEventListener("click", addToSeries)
+
+
+
+
+function storeInput() {
+    const blogPost = [];
+    let posts = JSON.parse(localStorage.getItem('post'));
+    if (posts === null) {
+         posts = [];
+    };
+    if (posts.length > 4) {
+         posts.length = 0;
+    }
+    var username = document.getElementById('username').value;
+    var title = document.getElementById('post-title').value;
+    var content = document.getElementById('post-content').value;
+    const newPost = [username, title, content]
+    if (posts === null) {
+         blogPost.push(newPost);
+         localStorage.setItem('post',JSON.stringify(blogPost));
+    }
+    else {
+         posts.push(newPost);
+         localStorage.setItem("post", JSON.stringify(posts));   
+    }
+    if (username !== '' && title !== '' && content !== '') {
+         window.location.assign('blog.html')
+    } else {
+         alert("Please enter your Username, Title, and Content to proceed")
+    }
+
 }
-
-document.getElementById(`submit`).addEventListener("click", addToSeries)
